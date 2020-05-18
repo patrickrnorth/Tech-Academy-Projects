7a20d757b863a07ef28608665a1a460ef302cbcb
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-18 17:53:28 -0400

    update AcmeAccounting

--- Baisc_CSharp_Projects/AcmeAccounting/AcmeAccounting/Program.cs
+++ Baisc_CSharp_Projects/AcmeAccounting/AcmeAccounting/Program.cs
@@ -7,7 +7,8 @@ namespace AcmeAccounting
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Acme Accounting Systems");
-            Console.WriteLine("Remember, We're \"accounting\" on you!"); 
+            Console.WriteLine("Remember, We're \"accounting\" on you!");
+            Console.ReadLine();
         }
     }
 }
