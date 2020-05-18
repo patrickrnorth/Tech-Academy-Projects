6f2e120132823014958628d9578fc97914fa9dbb
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-18 17:49:09 -0400

    add C# folders and Acme Accounting files

---
+++ Baisc_CSharp_Projects/AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, We're \"accounting\" on you!"); 
+        }
+    }
+}
