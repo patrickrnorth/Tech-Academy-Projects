43ed01a4a64ca5c4a9def097e2928770ead7e58d
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-19 15:32:19 -0400

    add BooleanLogic2.cs

---
+++ Baisc_CSharp_Projects/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false  ^ false);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Baisc_CSharp_Projects/BooleanLogic2/BooleanLogic2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BooleanLogic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            //Drivers Age
+            Console.WriteLine("What is your age?");
+            string ageStr = Console.ReadLine();
+            int age = Convert.ToInt16(ageStr);
+
+            //Drivers DUI record
+            Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false\".");
+            string duiStr = Console.ReadLine();
+            bool dui = Convert.ToBoolean(duiStr);
+
+            //Drivers Speeding record
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedStr = Console.ReadLine();
+            int speed = Convert.ToInt16(speedStr);
+
+            //Is driver qualified for insurance
+            bool qualified = (age > 15 && dui == false && speed <= 3);
+            Console.WriteLine("Is driver eligable for car insurance?");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
