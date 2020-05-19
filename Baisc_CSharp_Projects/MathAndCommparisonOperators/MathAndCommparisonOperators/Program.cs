e348d7c4e6fdd08b893f0277afd8cdf4e7139c74
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-19 14:31:49 -0400

    add MathAssignment.cs

---
+++ Baisc_CSharp_Projects/MathAndCommparisonOperators/MathAndCommparisonOperators/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MathAndCommparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemperature = 68;
+            int currentTemperature = 70;
+            //bool isWarm = currentTemperature <= roomTemperature;
+            bool isWarm = currentTemperature != roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Baisc_CSharp_Projects/MathAssignment/MathAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MathAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Multiply an input by 50
+            Console.WriteLine("Please input a number to multiply by 50: ");
+            string multiplyString = Console.ReadLine();
+            int multiplyInt = Convert.ToInt16(multiplyString);
+            Console.WriteLine(multiplyInt * 50);
+            
+            //Add an input by 25
+            Console.WriteLine("Please input a number to add 25: ");
+            string addString = Console.ReadLine();
+            int addInt = Convert.ToInt16(addString);
+            Console.WriteLine(multiplyInt + 25);
+            
+            //Divide an input by 12.5
+            Console.WriteLine("Please input a number to divide by 12.5: ");
+            string divideString = Console.ReadLine();
+            float divideInt = Convert.ToInt32(divideString);
+            Console.WriteLine(divideInt / 12.5);
+
+            //Greater than 50
+            Console.WriteLine("Please input a number to determine if its greater than 50: ");
+            string boolString = Console.ReadLine();
+            int boolInt = Convert.ToInt32(boolString);
+            int boolFifty = 50;
+            Console.WriteLine(boolInt > boolFifty);
+
+            //Remainder of dividing by 7
+            Console.WriteLine("Please input a number to determine its remainder divided by 7: ");
+            string remainderIntString = Console.ReadLine();
+            int remainderInt = Convert.ToInt16(remainderIntString);
+            Console.WriteLine(remainderInt % 7);
+        }
+    }
+}
