d24f7774a2d8029cab21efeb28b9319b10ac18a2
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-19 13:45:57 -0400

    add TTADailyReport.cs

---
+++ Baisc_CSharp_Projects/TTADailyReport/TTADailyReport/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TTADailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome message
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            
+            //Ask what course
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            
+            //Ask what page number
+            Console.WriteLine("What page number?");
+            string pageNumString = Console.ReadLine();
+            short pageNumShort = Convert.ToInt16(pageNumString);
+
+            //User help?
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\" ");
+            string needHelpString = Console.ReadLine();
+            bool needHelpBool = Convert.ToBoolean(needHelpString);
+
+            //Positive Experiences?
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string posExp = Console.ReadLine();
+
+            //Feedback?
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            //Hours studied
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudyString = Console.ReadLine();
+            short hoursStudyInt = Convert.ToInt16(hoursStudyString);
+
+            //End message
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Baisc_CSharp_Projects/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number? ");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNumb = Convert.ToInt32(favoriteNumber);
+            //int total = favNumb + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.352342;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Ryan";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+
+
+
+        }
+    }
+}
