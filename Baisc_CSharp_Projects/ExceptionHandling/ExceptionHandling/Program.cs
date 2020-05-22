df8372bb1eaa7cce9e7655106a94e1bb17720b68
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-22 14:06:10 -0400

    add ExceptionHanlingAssign.cs

---
+++ Baisc_CSharp_Projects/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+            
+
+
+            
+        }
+    }
+}
---
+++ Baisc_CSharp_Projects/ExceptionHandlingAssign/ExceptionHandlingAssign/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandlingAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try to catch any errors
+            try
+            {
+                //Create list of numbers
+                List<int> myNums = new List<int>();
+                myNums.Add(10);
+                myNums.Add(20);
+                myNums.Add(30);
+                myNums.Add(40);
+                myNums.Add(50);
+                myNums.Add(60);
+                myNums.Add(70);
+                myNums.Add(80);
+                myNums.Add(90);
+                myNums.Add(100);
+
+                //prompt user to input a dividend
+                Console.WriteLine("Please enter a number to divide the List of numbers by: ");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+
+
+                //loop will divide that number and print the result
+                foreach (int num in myNums)
+                {
+                    int dividedNum = num / userNum;
+                    Console.WriteLine(num + " divided by " + userNum + " is: " + dividedNum);
+                }
+                Console.ReadLine();
+            }
+            //catch blocks for my code
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            { 
+                Console.WriteLine("Try/Catch block has run, proceeding with program execution.");
+            }
+        }
+    }
+}
