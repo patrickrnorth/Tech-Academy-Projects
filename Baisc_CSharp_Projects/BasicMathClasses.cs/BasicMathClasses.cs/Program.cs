738e04b8ccfb96e6b31b3b582f766dd15ae7c46b
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 13:37:16 -0400

    add BasicMathClasses.cs assignment

---
+++ Baisc_CSharp_Projects/BasicMathClasses.cs/BasicMathClasses.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicMathClasses.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer to perfrom math opperations on: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            
+            int newNumAdd = mathFunc.addInt(userNum);
+            int newNumSub = mathFunc.subInt(userNum);
+            int newNumMult = mathFunc.multInt(userNum);
+
+            Console.WriteLine("Your number plus 10 is: " + newNumAdd);
+            Console.WriteLine("Your number subtracted by 5 is: " + newNumSub);
+            Console.WriteLine("Your number multiplied by 10 is: " + newNumMult);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Baisc_CSharp_Projects/BasicMathClasses.cs/BasicMathClasses.cs/mathFunc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BasicMathClasses.cs
+{
+    class mathFunc
+    {
+        public static int addInt(int num)
+        {
+            int myNum = num;
+            int myNumAdd = myNum + 10;
+            return myNumAdd;
+        }
+
+        public static int subInt(int num)
+        {
+            int myNum = num;
+            int myNumSub = myNum - 5 ;
+            return myNumSub;
+        }
+
+        public static int multInt(int num)
+        {
+            int myNum = num;
+            int myNumMult = myNum * 10;
+            return myNumMult;
+        }
+    }
+}
