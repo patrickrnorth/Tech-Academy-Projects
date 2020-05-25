2603d4bd5563b1c4e43df2984d705a20842c85d9
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 18:28:57 -0400

    add Dealer class

--- Baisc_CSharp_Projects/TwentyOne/TwentyOne/Program.cs
+++ Baisc_CSharp_Projects/TwentyOne/TwentyOne/Program.cs
@@ -10,7 +10,7 @@ namespace TwentyOne
     class Program
     {
         static void Main(string[] args)
-        {                        
+        {            
             Deck deck = new Deck();
             deck.Shuffle(3);
 
