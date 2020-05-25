d6b97228c2b5dc6fec2d2e1e4b283b145b3250de
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 18:10:24 -0400

    add Inheritance assignment

---
+++ Baisc_CSharp_Projects/Inheritance/Inheritance/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Employee : Person
+    {
+       public int id { get; set; }        
+    }
+}
---
+++ Baisc_CSharp_Projects/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciate new employee and set their name
+            Employee person1 = new Employee() { FirstName = "Sample", LastName = "Student" } ;
+            //Call the inherited person method to display the Employee name
+            person1.SayName();
+        }
+    }
+}
