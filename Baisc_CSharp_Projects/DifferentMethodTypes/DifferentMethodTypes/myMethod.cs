996047b7b482a741afd30728237984cf7cac9195
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 16:28:10 -0400

    add DifferentMethodTypes assignment

---
+++ Baisc_CSharp_Projects/DifferentMethodTypes/DifferentMethodTypes/myMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DifferentMethodTypes
+{
+    public class myMethod
+    {
+        public void myMethodOp(int userNum)
+        {
+            Console.WriteLine("Your number divided by two is: " + (userNum / 2));
+        }
+
+        public static int myMethodOp(int userNum, int myNum)
+        {
+            int newNum = userNum + myNum;
+            return newNum;
+        }
+
+
+    }
+}
