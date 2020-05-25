a211ab4f991b81c62bbb88a40fc33827bab58a36
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 15:16:48 -0400

    add MultipleParameters assignment.

---
+++ Baisc_CSharp_Projects/MultipleParameters/MultipleParameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultipleParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please input a second integer (optional): ");
+            bool userNum2IsValid = int.TryParse(Console.ReadLine(), out int userNum2);
+
+            int result = 0;
+            if (userNum2IsValid)
+            {
+                result = myMath.AddInts(userNum, userNum2);
+            }
+            else
+            {
+                result = myMath.AddInts(userNum);
+            }
+                       
+
+            Console.WriteLine("Your result is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Baisc_CSharp_Projects/MultipleParameters/MultipleParameters/myMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleParameters
+{
+    public class myMath
+    {
+        public static int AddInts(int userNum, int userNum2 = 0)
+        {
+            int newUserNum = userNum + userNum2;
+            return newUserNum;
+        }
+    }
+}
