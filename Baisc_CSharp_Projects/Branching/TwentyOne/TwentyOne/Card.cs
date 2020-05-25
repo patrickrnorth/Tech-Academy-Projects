a42a17197a32603ab4a896a8e796201a58bf5a29
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 13:50:33 -0400

    add TwentyOne.cs

---
+++ Baisc_CSharp_Projects/Branching/TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Face { get; set; }        
+    }
+}
