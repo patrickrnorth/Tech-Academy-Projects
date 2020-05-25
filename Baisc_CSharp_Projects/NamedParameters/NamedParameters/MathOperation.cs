efc11cae6df6f4c8950fbaf86379de5c3c6c1783
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 15:45:32 -0400

    add NamedPerameters assignment.

---
+++ Baisc_CSharp_Projects/NamedParameters/NamedParameters/MathOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamedParameters
+{
+    public class MathOperation
+    {
+        public static void myOperation(int firstNum, int secondNum)
+        {
+            int myNumAdd = firstNum + 10;
+            Console.WriteLine("The second number is: " + secondNum);
+        }
+    }
+}
---
+++ Baisc_CSharp_Projects/NamedParameters/NamedParameters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 10;
+            int myNum2 = 12;
+            MathOperation.myOperation(myNum, myNum2);
+            MathOperation.myOperation(secondNum: myNum2, firstNum: myNum);
+        }
+    }
+}
