e7bc5a6754ab4c0fa020ddb0fa523c4ac15529dc
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-25 14:47:53 -0400

    add ClassOverloading.cs assignment.

---
+++ Baisc_CSharp_Projects/ClassOverloading/ClassOverloading/MathOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassOverloading
+{
+    public class MathOperations
+    {
+        //will receive int parameter and do an addition operation on it and return as an integer.
+        public static int myOpperation(int userNum)
+        {
+            int myNum = userNum;
+            int myNumAdd = myNum + 10;
+            return myNumAdd;
+        }
+
+        //will receive decimal parameter and do an multiplication operation on it and return as an integer .
+        public static int myOpperation(float userNum)
+        {
+            int myNum = Convert.ToInt32(userNum);
+            int myNumMult = myNum * 10;
+            return myNumMult;
+        }
+
+        public static int myOpperation(string userNum)
+        {
+            int myNum = Convert.ToInt32(userNum);
+            int myNumDivide = myNum / 10;
+            return myNumDivide;
+        }
+        
+    }
+}
---
+++ Baisc_CSharp_Projects/ClassOverloading/ClassOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an integer to operate on: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            int newNum = MathOperations.myOpperation(userNum);
+
+            Console.WriteLine("Your integer plus 10 is: " + newNum);
+            Console.ReadLine();
+
+            Console.WriteLine("Please input a decimal to operate on: ");
+            float userNumFloat = float.Parse(Console.ReadLine());
+
+            float newNumFloat = MathOperations.myOpperation(userNumFloat);
+
+            Console.WriteLine("Your decimal multiplied by 10 is: " + newNumFloat);
+            Console.ReadLine();
+
+            Console.WriteLine("Please input a integer to operate on: ");
+            int newNumDivide = MathOperations.myOpperation(Console.ReadLine());
+
+            Console.WriteLine("Your integer divided by 10 is: " + newNumDivide);
+            Console.ReadLine();
+        }
+    }
+}
