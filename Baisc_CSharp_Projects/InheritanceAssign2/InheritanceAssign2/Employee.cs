4f6f9aa443ef28b86089fdb0e0785cd6c64f9db7
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-26 19:01:04 -0400

    update InheritanceAssign2

--- Baisc_CSharp_Projects/InheritanceAssign2/InheritanceAssign2/Employee.cs
+++ Baisc_CSharp_Projects/InheritanceAssign2/InheritanceAssign2/Employee.cs
@@ -6,42 +6,44 @@ using System.Text;
 
 namespace InheritanceAssign2
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person//, IQuittable
     {
         public int Id { get; set; }
+
+        public List<T> Things { get; set; }
         
-        public void Quit(Employee employee)
-        {
-            Console.WriteLine(employee.FirstName + " " + employee.LastName + " has quit.");
-        }
-        public static bool operator==(Employee employee1, Employee employee2)
-        {
-            if(employee1.Id == employee2.Id)
-                return true;
-            else
-                return false;
-        }
-        public static bool operator!=(Employee employee1, Employee employee2)
-        {
-            if (employee1.Id != employee2.Id)
-                return true;
-            else
-                return false;
-        }
-        public static bool operatorEquals(Employee employee1, Employee employee2)
-        {
-            if (employee1.Id == employee2.Id)
-                return true;
-            else
-                return false;
-        }
-        public static bool operatorGetHashCode(Employee employee1, Employee employee2)
-        {
-            if (employee1.Id == employee2.Id)
-                return true;
-            else
-                return false;
-        }
+        //public void Quit(Employee employee)
+        //{
+        //    Console.WriteLine(employee.FirstName + " " + employee.LastName + " has quit.");
+        //}
+        //public static bool operator==(Employee employee1, Employee employee2)
+        //{
+        //    if(employee1.Id == employee2.Id)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        //public static bool operator!=(Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id != employee2.Id)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        //public static bool operatorEquals(Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id == employee2.Id)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        //public static bool operatorGetHashCode(Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id == employee2.Id)
+        //        return true;
+        //    else
+        //        return false;
+        //}
 
     }
 }
--- Baisc_CSharp_Projects/InheritanceAssign2/InheritanceAssign2/Program.cs
+++ Baisc_CSharp_Projects/InheritanceAssign2/InheritanceAssign2/Program.cs
@@ -6,27 +6,49 @@ namespace InheritanceAssign2
     {
         static void Main(string[] args)
         {
-            Employee person1 = new Employee();
-            Employee person2 = new Employee();
-            person1.FirstName = "Sample";
-            person1.LastName = "Student";
-            person1.Id = Convert.ToInt32("1234");
-            person1.SayName();
-
-            person2.FirstName = "Bill";
-            person2.LastName = "North";
-            person2.Id = Convert.ToInt32("1234");
-            person2.SayName();
-
-            if (person1 == person2)
-                Console.WriteLine("The two employee's have the same ID.");
-            else
-                Console.WriteLine("The two employee's have different ID's");
-
+            Employee<string> stuff = new Employee<string>();
             
-            //Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
-            person1.Quit(person1);
-            person2.Quit(person2);
+            stuff.Things.Add("Books");
+            stuff.Things.Add("Pool");
+            stuff.Things.Add("Bike");            
+
+            Employee<int> otherStuff = new Employee<int>();
+            //otherStuff.Things = new List<int>();
+            otherStuff.Things.Add(10);
+            otherStuff.Things.Add(29);
+            otherStuff.Things.Add(4);
+
+            for (int i = 0; i < stuff.Things.Count; i++)
+            {
+                Console.WriteLine(stuff.Things);
+                Console.WriteLine(otherStuff.Things);
+            }
+            
+
+
+
+            //Employee person1 = new Employee();
+            //Employee person2 = new Employee();
+
+            //person1.FirstName = "Sample";
+            //person1.LastName = "Student";
+            //person1.Id = Convert.ToInt32("1234");
+            //person1.SayName();
+
+            //person2.FirstName = "Bill";
+            //person2.LastName = "North";
+            //person2.Id = Convert.ToInt32("1234");
+            //person2.SayName();
+
+            //if (person1 == person2)
+            //    Console.WriteLine("The two employee's have the same ID.");
+            //else
+            //    Console.WriteLine("The two employee's have different ID's");
+
+
+            ////Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+            //person1.Quit(person1);
+            //person2.Quit(person2);
         }
     }
 }
