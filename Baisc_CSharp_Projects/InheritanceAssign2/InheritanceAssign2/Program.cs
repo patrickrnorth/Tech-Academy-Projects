1a6dd93ed450afd48b8eed5e5f53bfb0cb96c83b
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-26 19:33:34 -0400

    add InheritanceAssign2 with generic Employee Class.

--- Baisc_CSharp_Projects/InheritanceAssign2/InheritanceAssign2/Program.cs
+++ Baisc_CSharp_Projects/InheritanceAssign2/InheritanceAssign2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InheritanceAssign2
 {
@@ -15,35 +16,35 @@ namespace InheritanceAssign2
             
             for (int i = 0; i < stuff.Things.Count; i++)
             {
-                Console.WriteLine(stuff.Things[i]);
+                Console.WriteLine(stuff.Things[i]);                
+            }
+            for (int i = 0; i < stuff.Things.Count; i++)
+            {                
                 Console.WriteLine(otherStuff.Things[i]);
             }
-            
+
+        //    employee person1 = new employee();
+        //    employee person2 = new employee();
+
+        //    person1.firstname = "sample";
+        //    person1.lastname = "student";
+        //    person1.id = convert.toint32("1234");
+        //    person1.sayname();
+
+        //    person2.firstname = "bill";
+        //    person2.lastname = "north";
+        //    person2.id = convert.toint32("1234");
+        //    person2.sayname();
+
+        //    if (person1 == person2)
+        //        console.writeline("the two employee's have the same id.");
+        //    else
+        //        console.writeline("the two employee's have different id's");
 
 
-
-            //Employee person1 = new Employee();
-            //Employee person2 = new Employee();
-
-            //person1.FirstName = "Sample";
-            //person1.LastName = "Student";
-            //person1.Id = Convert.ToInt32("1234");
-            //person1.SayName();
-
-            //person2.FirstName = "Bill";
-            //person2.LastName = "North";
-            //person2.Id = Convert.ToInt32("1234");
-            //person2.SayName();
-
-            //if (person1 == person2)
-            //    Console.WriteLine("The two employee's have the same ID.");
-            //else
-            //    Console.WriteLine("The two employee's have different ID's");
-
-
-            ////Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
-            //person1.Quit(person1);
-            //person2.Quit(person2);
+        //    //use polymorphism to create an object of type iquittable and call the quit() method on it.
+        //    person1.quit(person1);
+        //    person2.quit(person2);
         }
     }
 }
