76ca921348b9c8c5f12e099186deb01ace71aafa
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-05-29 17:13:56 -0400

    update ConstVarChainConstructors

--- Baisc_CSharp_Projects/ConstVarChainConstructors/ConstVarChainConstructors/Program.cs
+++ Baisc_CSharp_Projects/ConstVarChainConstructors/ConstVarChainConstructors/Program.cs
@@ -11,20 +11,6 @@ namespace ConstVarChainConstructors
         {
             const string myName = "Ryan";
             var primeNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-          
         }
-
-        public string FirstName;
-        public string LastName;
-        public void Person(string firstName) : this(firstName, "")
-        {
-
-        }
-        public void Person(string firstName, string lastName)
-        {
-            LastName = lastName;
-            FirstName = firstName;
-        }
-
     }
 }
