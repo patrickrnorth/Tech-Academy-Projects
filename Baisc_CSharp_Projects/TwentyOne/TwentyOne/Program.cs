cca4299ab09c477f6e772e327eb824547f119978
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-06-01 16:24:53 -0400

    update TwentyOne

--- Baisc_CSharp_Projects/TwentyOne/TwentyOne/Program.cs
+++ Baisc_CSharp_Projects/TwentyOne/TwentyOne/Program.cs
@@ -2,6 +2,7 @@
 using Casino;
 using Casino.TwentyOne;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace TwentyOne
 {
@@ -9,8 +10,9 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            const string casinoName = "Grand Hotel and Casino";
+
+            Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
             
             bool validAnswer = false;
@@ -30,7 +32,7 @@ namespace TwentyOne
             {
                 Player player = new Player(playerName, bank);
                 player.Id = Guid.NewGuid();
-                using (StreamWriter file = new StreamWriter(@"C:\Users\Public\Documents\log.txt", true))//cleans up memory
+                using (StreamWriter file = new StreamWriter(@"C:\Users\Public\Documents\log.txt", true))
                 {
                     file.WriteLine(player.Id);                    
                 }
@@ -43,14 +45,15 @@ namespace TwentyOne
                     try
                     {
                         game.Play();
-                    }
-                    
-                    /*catch(FraudException)  ERROR CODE CS0122 INACCESSIBLE DUE TO ITS PROTECTION LEVEL.
+                    }                    
+                    catch(FraudException ex)
                     {
                         Console.WriteLine("Security! Kick this person out.");
                         Console.ReadLine();
+                        updateDbWithException(ex);
+                        Console.ReadLine();
                         return;
-                    }*/
+                    }
                     catch (Exception)
                     {
                         Console.WriteLine("An error occured. Please contact your System Administrator.");
@@ -63,6 +66,30 @@ namespace TwentyOne
             }
             Console.WriteLine("Feel free to look around the casino. Bye for now!");
             Console.ReadLine();
+        }
+
+        private static void updateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog = TwentyOneGame; Integrated Security = True; Connect Timeout = 30;
+                                        Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES (@ExceptionType, @ExceptionMessage, @TimeStamp)"; 
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", System.Data.SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", System.Data.SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", System.Data.SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
         }
     }
 }
