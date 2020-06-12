83da4b341929a2f57ee3edd2a69729e223840ab9
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-06-12 16:10:28 -0400

    add MyPortfolio
    Skeleton of the project

---
+++ MyWebPortfolio/JS/myPortfolio.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
