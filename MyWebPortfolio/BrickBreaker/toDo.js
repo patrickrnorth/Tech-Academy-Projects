08f7bb27083d5786752a8df46a8ce7f428278ac9
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-06-17 21:25:44 -0400

    add BrickBreaker
    add html/js for brickbreaker game, need to add score function

---
+++ MyWebPortfolio/BrickBreaker/toDo.js
@@ -0,0 +1,25 @@
+let todoItems = [];
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  console.log(todoItems);
+}
+
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('.js-todo-input');
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
